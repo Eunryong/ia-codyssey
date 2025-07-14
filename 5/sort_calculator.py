d0cc721ebb1d01e2489cac34aacae50099444299
Eunryong <srdn45@gmail.com>
2025-07-15 02:16:53 +0900

    feat:quicksort and staging_commit

---
+++ 5/sort_calculator.py
@@ -0,0 +1,20 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    tail = arr[1:]
+    left = [x for x in tail if x <= pivot]
+    right = [x for x in tail if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+def main():
+    try:
+        numbers = list(map(float, input().split()))
+        sorted_numbers = quick_sort(numbers)
+        print("Sorted:", sorted_numbers)
+
+    except ValueError:
+        print("Invalid input.")
+
+if __name__ == "__main__":
+    main()
