a7f92b96296c2b3e3c5db6bc21fc4ddc6ef6841d
Eunryong <srdn45@gmail.com>
2025-07-16 14:15:19 +0900

    feat:app.py

--- 7/david/app.py
+++ 7/david/app.py
@@ -5,28 +5,26 @@ from gtts import gTTS
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def get():
-
-    return render_template('index.html', title="Flask Example", name="은룡")
-    
-@app.route("/", methods=['POST'])
+@app.route("/")
 def post():
-    try:
-        text = request.form.get('input_text', '').strip()
-        lang = request.form.get('lang', 'ko')
+    if request.method == 'GET':
+        return render_template('index.html', title="Flask Example", name="은룡")
+    if request.method == 'POST':
+        try:
+            text = request.form.get('input_text', '').strip()
+            lang = request.form.get('lang', 'ko')
 
-        if not text:
-            return jsonify({"error": "텍스트가 비어 있습니다."}), 400
+            if not text:
+                return jsonify({"error": "텍스트가 비어 있습니다."}), 400
 
-        fp = BytesIO()
-        tts = gTTS(text=text, lang=lang, tld="com")
-        tts.write_to_fp(fp)
-        fp.seek(0)
-        return Response(fp.getvalue(), mimetype='audio/mpeg')
-    
-    except Exception as e:
-        return jsonify({"error": f"gTTS 변환 실패: {str(e)}"}), 500
+            fp = BytesIO()
+            tts = gTTS(text=text, lang=lang, tld="com")
+            tts.write_to_fp(fp)
+            fp.seek(0)
+            return Response(fp.getvalue(), mimetype='audio/mpeg')
+        
+        except Exception as e:
+            return jsonify({"error": f"gTTS 변환 실패: {str(e)}"}), 500
 
 
 if __name__ == '__main__':
